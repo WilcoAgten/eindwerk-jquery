ffbffb69fa6a8c82b893f8a0e04d72b189164302
WilcoAgten <wilcoagten@hotmail.com>
2023-04-25 11:47:34 +0200

    add header and main content

--- js/eindwerk.js
+++ js/eindwerk.js
@@ -1,4 +1,28 @@
 $(document).ready(function () {
     
+    //wilco
+    let accordion = $('#accordion');
+
+    $.getJSON("chapters.json", function (data) {
+        $.each(data, function (i, chapter) {
+          let line =
+          "<h3>" + chapter.name + " (" + chapter.duration + "uur)" + "</h3><div><p>" + chapter.description+ "</p></div>";
+          $("#accordion").append(line);
+        });
+
+        accordion.accordion({
+            active: true,
+            collapsible: true,
+            icons: { "header": "ui-icon-plus", "activeHeader": "ui-icon-minus" }
+        });
+      });
+
+      
+    
+    
+
+
+    
+    //einde wilco
     
 });
